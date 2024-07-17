942d6ead9424359e35c47f912243b0e8829a813c
Dionne Memie <d.memie@gmail.com>
2024-07-17 16:03:36 +0100

    JS started

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Hello world")
+
+// Register answer as correct/incorrect
+// When submit clicked 
+    // If answer is correct, correct result screen shows
+    // If incorrect, fail screen shows 
+
+        if answer === "Paris" {}
