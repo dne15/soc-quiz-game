3ef2ed504c0896c155c38e3d58f3387515e246ff
Ryan <ryanshaunsmith@gmail.com>
2024-07-18 11:36:32 +0100

    completed logic to switch to the fail or success screen depending on option chosen

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Register answer as correct/incorrect
 // When submit clicked 
-// If answer is correct, correct result screen shows
+
 // If incorrect, fail screen shows 
 
 // grab quiz for to manipulate answer values
 const quizForm = document.querySelector(".answerOptions");
 // hardcode answer to be paris
-const correctAnswer = "paris";
+const correctAnswer = "berlin";
 // grab question screen
 const questionScreen = document.querySelector(".screen-question")
 // grab correct screen
@@ -14,18 +14,28 @@ const correctScreen = document.querySelector(".screen-correct");
 // grab fail screen
 const failScreen = document.querySelector(".screen-fail");
 
-const selectedInput = document.querySelector("input:checked");
-
-// Has answer been selected before pressing submit?
 
 function handleSubmit(e) {
     e.preventDefault();
-    if (!selectedInput) {
-    alert("You have not selected an answer")
-    return;
-}
 
-    if selectedInput
+    const selectedInput = document.querySelector("input[name='answers']:checked");
+    // Has answer been selected before pressing submit?
+    if (!selectedInput) {
+        alert("You have not selected an answer")
+        return;
+    }
+
+    // storing selected options value as an answer to compare to the Correct answer
+    const selectedAnswer = selectedInput.value;
+
+    // If answer is correct, correct result screen shows
+    if (selectedAnswer === correctAnswer) {
+        questionScreen.classList.add("hide");
+        correctScreen.classList.remove("hide");
+    } else {
+        questionScreen.classList.add("hide");
+        failScreen.classList.remove("hide");
+    }
 
 }
 // add event listener
